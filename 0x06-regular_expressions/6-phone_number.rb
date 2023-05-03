763b3393b98b351cc14f169e657b31a05ac99a5a
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-04 01:47:09 +0200

    Update 6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^[0-9]{10}$/).join
+# This script checks if a string contains a 10 digit phone number.
+
+input_string = ARGV[0]
+if input_string.match(/^\d{10}$/)
+  puts "Match found!"
+else
+  puts "No match found."
+end
