90000f4e638a95cd5209a8a4e7120a5710b8480c
genyarko <115112347+genyarko@users.noreply.github.com>
2023-05-04 01:48:40 +0200

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+# This script parses a log message and extracts the sender, receiver, and flags information.
+
+input_string = ARGV[0]
+regex_pattern = /(?<sender>[^,]+),(?<receiver>[^,]+),(?<flags>[^\s]+)/
+
+match_data = input_string.match(regex_pattern)
+
+if match_data
+  puts "Sender: #{match_data[:sender]}"
+  puts "Receiver: #{match_data[:receiver]}"
+  puts "Flags: #{match_data[:flags]}"
+else
+  puts "No match found."
+end
