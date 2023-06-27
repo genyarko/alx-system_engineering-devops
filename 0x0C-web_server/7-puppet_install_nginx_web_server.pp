b569504012f3f850ab1192ffb65ea8285d09fbfa
genyarko <115112347+genyarko@users.noreply.github.com>
2023-06-27 17:53:03 +0200

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,51 +1,20 @@
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => '# Redirect /redirect_me to a different page
-                server {
-                  listen 80 default_server;
-                  listen [::]:80 default_server;
-
-                  root /var/www/html;
-                  index index.html;
-
-                  server_name _;
-
-                  location = /redirect_me {
-                    return 301 https://example.com/newpage;
-                  }
-
-                  location / {
-                    try_files $uri $uri/ =404;
-                  }
-                }',
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  file { '/etc/nginx/sites-enabled/default':
-    ensure  => 'link',
-    target  => '/etc/nginx/sites-available/default',
-    require => File['/etc/nginx/sites-available/default'],
-    notify  => Service['nginx'],
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    require   => Package['nginx'],
-    subscribe => [File['/etc/nginx/sites-available/default'], File['/etc/nginx/sites-enabled/default']],
-  }
+# Install Nginx web server (w/ Puppet)
+package { 'nginx':
+  ensure => installed,
 }
 
-class { 'nginx': }
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://example.com/new_page permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
