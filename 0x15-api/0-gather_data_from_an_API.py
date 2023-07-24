36e448ff69f43d5d72fb870cf27da96fe5639374
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-24 17:19:06 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,48 +1,14 @@
 #!/usr/bin/python3
-"""
-Module: gather_data_from_an_API
-
-Script to fetch TODO list progress for a given employee ID using REST API.
-
-Usage:
-    python3 gather_data_from_an_API.py <employee_id>
-"""
-
-import sys
+"""Returns to-do list information for a given employee ID."""
 import requests
-
-def get_employee_todo_list_progress(employee_id):
-    """
-    Fetch and display the TODO list progress for the given employee ID.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        None
-    """
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    todos_url = f'{base_url}/todos?userId={employee_id}'
-
-    response_user = requests.get(user_url)
-    response_todos = requests.get(todos_url)
-
-    user_data = response_user.json()
-    todos_data = response_todos.json()
-
-    employee_name = user_data['name']
-    total_tasks = len(todos_data)
-    completed_tasks = [task for task in todos_data if task['completed']]
-
-    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_list_progress(employee_id)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
