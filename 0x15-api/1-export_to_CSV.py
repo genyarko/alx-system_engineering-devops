cb8f9c1eb686646037c2dd02a0785c8956ec93b4
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-24 17:25:21 +0200

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,46 +1,52 @@
 #!/usr/bin/python3
 """
 Module: gather_data_from_an_API
-
 Script to fetch TODO list progress for a given employee ID using REST API and export data to CSV format.
-
 Usage:
     python3 gather_data_from_an_API.py <employee_id>
 """
-def get_employee_todos(user_id):
-    url = "https://jsonplaceholder.typicode.com/"
-    user_response = requests.get(url + "users/{}".format(user_id))
-    todos_response = requests.get(url + "todos", params={"userId": user_id})
 
-    if user_response.status_code != 200 or todos_response.status_code != 200:
-        print("Error: Unable to fetch data from the API.")
-        sys.exit(1)
+import sys
+import requests
+import csv
 
-    user = user_response.json()
-    todos = todos_response.json()
-    return user, todos
+def get_employee_todo_list_progress(employee_id):
+    """
+    Fetch and display the TODO list progress for the given employee ID and export to CSV format.
+    Args:
+        employee_id (int): The ID of the employee.
+    Returns:
+        None
+    """
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{base_url}/users/{employee_id}'
+    todos_url = f'{base_url}/todos?userId={employee_id}'
+
+    response_user = requests.get(user_url)
+    response_todos = requests.get(todos_url)
+
+    user_data = response_user.json()
+    todos_data = response_todos.json()
+
+    employee_name = user_data['name']
+
+    csv_file = f'{employee_id}.csv'
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        for task in todos_data:
+            task_id = task['id']
+            task_title = task['title']
+            task_status = "COMPLETED" if task['completed'] else "INCOMPLETE"
+            writer.writerow([task_id, employee_name, task_status, task_title])
+
+    print(f"Employee {employee_name} data exported to {csv_file}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+    if len(sys.argv) != 2:
         print("Usage: python3 gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
-    user, todos = get_employee_todos(employee_id)
-    username = user.get("username")
-
-    with open(f"{employee_id}.csv", "w", newline="") as csvfile:
-        fieldnames = ["Task ID", "Employee ID", "Employee Name", "Completed", "Title"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for todo in todos:
-            writer.writerow({
-                "Task ID": todo.get("id"),
-                "Employee ID": employee_id,
-                "Employee Name": username,
-                "Completed": todo.get("completed"),
-                "Title": todo.get("title")
-            })
-
-    print(f"Data for Employee {username} exported to {employee_id}.csv successfully.")
+    get_employee_todo_list_progress(employee_id)
