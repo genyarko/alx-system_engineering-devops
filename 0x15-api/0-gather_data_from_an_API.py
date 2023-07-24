70faeeb4a5c78388127793fa34e14f3337166081
genyarko <115112347+genyarko@users.noreply.github.com>
2023-07-24 17:21:58 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,15 +5,33 @@ Script to fetch TODO list progress for a given employee ID using REST API.
 Usage:
     python3 gather_data_from_an_API.py <employee_id>
 """
+
 import requests
 import sys
 
-if __name__ == "__main__":
+def get_employee_data(employee_id):
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user_response = requests.get(url + "users/{}".format(employee_id))
+    todos_response = requests.get(url + "todos", params={"userId": employee_id})
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Unable to fetch data from the API.")
+        sys.exit(1)
+
+    user = user_response.json()
+    todos = todos_response.json()
+
+    completed = [todo.get("title") for todo in todos if todo.get("completed")]
+    return user, completed, len(todos)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    user, completed, total_tasks = get_employee_data(employee_id)
+
+    print(f"Employee {user.get('name')} is done with tasks({len(completed)}/{total_tasks}):")
+    [print(f"\t {c}") for c in completed]
+
