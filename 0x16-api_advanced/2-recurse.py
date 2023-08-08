7cfd9e64bca51d68e5df296d0d61590dc178317c
genyarko <115112347+genyarko@users.noreply.github.com>
2023-08-08 18:44:17 +0200

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,9 +2,7 @@
 """Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
-def count_words(subreddit, word_list, after=None, counts=None):
-    if counts is None:
-        counts = {}
+def recurse(subreddit, hot_list=[], after=None):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "myBot/0.0.1"}
     params = {"limit": 100}
@@ -12,21 +10,12 @@ def count_words(subreddit, word_list, after=None, counts=None):
         params["after"] = after
     response = requests.get(url, headers=headers, params=params, allow_redirects=False)
     if response.status_code != 200:
-        return
+        return None
     data = response.json()
     posts = data["data"]["children"]
     for post in posts:
-        title = post["data"]["title"].lower()
-        for word in word_list:
-            if word.lower() in title and not any(c in word.lower() for c in ".!_"):
-                if word.lower() in counts:
-                    counts[word.lower()] += title.count(word.lower())
-                else:
-                    counts[word.lower()] = title.count(word.lower())
-    if not counts:
-        return
-    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-    for word, count in sorted_counts:
-        print(f"{word}: {count}")
+        hot_list.append(post["data"]["title"])
     after = data["data"]["after"]
-    count_words(subreddit, word_list, after, counts)
+    if after is None:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
