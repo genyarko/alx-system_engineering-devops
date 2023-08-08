bb3ba1feff229e4d297a8116ac2344f10fe2e6c9
genyarko <115112347+genyarko@users.noreply.github.com>
2023-08-08 18:39:11 +0200

    Update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
 """Function to query a list of all hot posts on a given Reddit subreddit."""
-import praw
 import requests
 
-def count_words(subreddit, word_list, reddit=None, counts=None, after=None):
-    if reddit is None:
-        reddit = praw.Reddit(user_agent='myBot/0.0.1')
+def count_words(subreddit, word_list, after=None, counts=None):
     if counts is None:
         counts = {}
-    try:
-        hot_posts = reddit.subreddit(subreddit).hot(limit=None, params={'after': after})
-    except praw.exceptions.NotFound:
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "myBot/0.0.1"}
+    params = {"limit": 100}
+    if after:
+        params["after"] = after
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code != 200:
         return
-    for post in hot_posts:
-        words = post.title.lower().split()
+    data = response.json()
+    posts = data["data"]["children"]
+    for post in posts:
+        title = post["data"]["title"].lower()
         for word in word_list:
-            if word.lower() in words:
+            if word.lower() in title and not any(c in word.lower() for c in ".!_"):
                 if word.lower() in counts:
-                    counts[word.lower()] += words.count(word.lower())
+                    counts[word.lower()] += title.count(word.lower())
                 else:
-                    counts[word.lower()] = words.count(word.lower())
+                    counts[word.lower()] = title.count(word.lower())
     if not counts:
         return
     sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
     for word, count in sorted_counts:
         print(f"{word}: {count}")
-    after = post.name
-    count_words(subreddit, word_list, reddit, counts, after=after)
-    
+    after = data["data"]["after"]
+    count_words(subreddit, word_list, after, counts)
